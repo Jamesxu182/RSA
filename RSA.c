439d34cb20b755f0de90d0a90a2b4ba7027cbedc
jamesxu182 <jamesxu182@gmail.com>
2016-03-10 23:14:23 +0000

    Add Test.py

--- RSA.c
+++ RSA.c
@@ -20,15 +20,44 @@
 #include <time.h>
 #include <math.h>
 
-void generateRandomNumber(int num[])
+/*
+ * =====================================================================================
+ *    Function: generateRandomNumber
+ *
+ * Description: generate a 128 bits binary integer number
+ *
+ *  Parameters: int num[]    array of integer
+ *
+ *              int length   the length of the random binary nubmer
+ *
+ *      Return: void
+ *
+ * =====================================================================================
+ */
+void generateRandomNumber(int num[], int length)
 {
     int i;
 
-    for(i = 0; i < 128; i++)
+    for(i = 0; i < length; i++)
     {
         num[i] = rand() % 2;
     }
 }
+
+/*
+ * =====================================================================================
+ *    Function: viewRandomeNumber
+ * 
+ * Description: display binary number on screen
+ *
+ *  Parameters: int num[]    binary number as integer number
+ *             
+ *             int length   the length of binary number
+ *
+ *      Return: void
+ * 
+ * =====================================================================================
+ */
 
 void viewRandomNumber(int num[], int length)
 {
@@ -45,7 +74,27 @@ void viewRandomNumber(int num[], int length)
     printf("\n");
 }
 
-int * mul(int num1[], int length_1, int num2[], int length_2)
+/*
+ * =====================================================================================
+ *    Function: mul
+ * 
+ * Description: calculate the result of two binary number
+ *
+ *  Parameters: int num1[]       the first binary number
+ *
+ *              int length_1     the lenght of first binary number
+ *
+ *              int num2[]       the second binary number
+ *
+ *              int length_2     the lenght of second binary number
+ *   
+ *              int * length_res the lenght of result
+ *
+ *      Return: res              the result of multiple                
+ *
+ * =====================================================================================
+ */
+int * mul(int num1[], int length_1, int num2[], int length_2, int * length_res)
 {
     int i, j;
     int length = length_1 + length_2;
@@ -71,27 +120,32 @@ int * mul(int num1[], int length_1, int num2[], int length_2)
         carry = (res[i] + carry) / 2;
     }
 
+    * length_res = length;
+
     return res;
 }
 
 int main()
 {
     time_t t;
-    int p[128];
-    int q[128];
+    int length_p = 3;
+    int length_q = 3;
+    int p[length_p];
+    int q[length_q];
     int * e;
+    int length_e;
 
     srand((unsigned) time(&t));
 
-    generateRandomNumber(p);
-    generateRandomNumber(q);
+    generateRandomNumber(p, length_p);
+    generateRandomNumber(q, length_q);
 
-    viewRandomNumber(p, 128);
-    viewRandomNumber(q, 128);
+    viewRandomNumber(p, length_p);
+    viewRandomNumber(q, length_q);
 
-    e = mul(p, 128, q, 128);
+    e = mul(p, length_p, q, length_q, &length_e);
 
-    viewRandomNumber(e, 256);
+    viewRandomNumber(e, length_e);
 
     return 0;
 }
